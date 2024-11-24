759fbb4099a796fd118ef98f7c62fa025b4796fa
Anna Yoon <annaeyoon9@gmail.com>
2024-11-24 01:20:04 -0800

    small change

--- WebContent/index.js
+++ WebContent/index.js
@@ -92,55 +92,9 @@ function handleSessionData(resultDataString) {
         // Append the cell to the current row
         currentRow.append(cellHTML);
     }
-
-
 }
 
-/**
- * Handle the items in item list
- * //@param resultArray jsonObject, needs to be parsed to html
- *
-function handleCartArray(resultArray) {
-    console.log(resultArray);
-    let item_list = $("#item_list");
-    // change it to html list
-    let res = "<ul>";
-    for (let i = 0; i < resultArray.size(); i++) {
-        // each item will be in a bullet point
-        res += "<li>" + resultArray[i] + "</li>";
-    }
-    res += "</ul>";
 
-    // clear the old array and show the new array in the frontend
-    item_list.html("");
-    item_list.append(res);
-}*/
-
-/**
- * Submit form content with POST method
- * //@param cartEvent
- *
-function handleCartInfo(cartEvent) {
-    console.log("submit cart form");
-    /**
-     * When users click the submit button, the browser will not direct
-     * users to the url defined in HTML form. Instead, it will call this
-     * event handler when the event is triggered.
-     *
-    cartEvent.preventDefault();
-
-    $.ajax("api/index", {
-        method: "POST",
-        data: cart.serialize(),
-        success: resultDataString => {
-            let resultDataJson = JSON.parse(resultDataString);
-            handleCartArray(resultDataJson["cartItems"]);
-        }
-    });
-
-    // clear input form
-    cart[0].reset();
-}*/
 
 
 $.ajax("api/index", {
