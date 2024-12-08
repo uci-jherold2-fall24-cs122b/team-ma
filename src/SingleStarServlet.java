28dcf5ba39ad5d1da4de980f9e1da551657a96ab
meerajagota <56693917+meerajagota@users.noreply.github.com>
2024-12-08 10:35:52 -0800

    Update SingleStarServlet.java

--- src/SingleStarServlet.java
+++ src/SingleStarServlet.java
@@ -29,7 +29,7 @@ public class SingleStarServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/MySQLReadOnly");
         } catch (NamingException e) {
             e.printStackTrace();
         }
