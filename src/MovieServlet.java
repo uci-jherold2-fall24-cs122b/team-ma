f57e5070fc08439fa8b23917194592b233531bcb
meerajagota <56693917+meerajagota@users.noreply.github.com>
2024-12-08 10:33:09 -0800

    Update MovieServlet.java

--- src/MovieServlet.java
+++ src/MovieServlet.java
@@ -34,7 +34,7 @@ public class MovieServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/MySQLReadOnly");
         } catch (NamingException e) {
             e.printStackTrace();
         }
