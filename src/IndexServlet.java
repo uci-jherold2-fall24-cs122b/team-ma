d74ec0fd71d83e0219f413e00d95831b7b0fe102
meerajagota <56693917+meerajagota@users.noreply.github.com>
2024-12-08 12:00:25 -0800

    change to readwrite

--- src/IndexServlet.java
+++ src/IndexServlet.java
@@ -28,7 +28,7 @@ public class IndexServlet extends HttpServlet {
 
     public void init(ServletConfig config) {
         try {
-            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/MySQLReadWrite");
         } catch (NamingException e) {
             e.printStackTrace();
         }
